a55bba73bffc968d813fa439c4f412eec7c48a2e
Michael Gaudreau <gaudreaum01@gmail.com>
2019-10-13 19:16:52 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,1 +1,8 @@
-class ChangeDatatypeForBirthdate
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  
+  def change 
+    add_column(:students, :grade, :integer)
+    add_column(:students, :birthday, :string)
+  end 
+  
+end 
